34987543bfcdf511071138745f00a16456493e63
Justin Good <justingood@gmail.com>
2017-11-29 18:55:38 +0000

    Grabbing the sensor data is suprisingly easy

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import dht
 import json
+import machine
+
 
 with open('config.json', 'r') as c:
     config = json.load(c)
+
+# Set up our sensor pins
+sensor = dht.DHT22(machine.Pin(config['sensor']['pin']))
+
+# Take our readings
+sensor.measure()
+
+print('The temperature is', sensor.temperature(), '°C')
+print('The humidity is', sensor.humidity(), '°C')
